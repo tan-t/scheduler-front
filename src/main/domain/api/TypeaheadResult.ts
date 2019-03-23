45cdff697283d9ebc0226dc8df163ade03420c8d
tan-t <lee30x@gmail.com>
2019-03-23 22:20:30 +0900

    clean repo

--- src/main/domain/api/TypeaheadResult.ts
+++ src/main/domain/api/TypeaheadResult.ts
@@ -1,13 +1,13 @@
 import * as moment from "../../../../node_modules/moment";
 
+moment.lang('ja');
 
 export default class TypeaheadResult { 
     value:moment.Moment;
     label:string;
 
     public static from(m:moment.Moment):TypeaheadResult {
-        moment.locale('ja');
-        return {value:m,label:m.toLocaleString()};
+        return {value:m,label:m.format("MM/DD (ddd)")};
     }
 
 }
