ce91abf3b2565c7bdef627e0bb1d6389b32e43eb
tan-t <lee30x@gmail.com>
2019-03-24 13:22:32 +0900

    set region to asia-northeast1

--- src/main/platform/firebase/typeahead.ts
+++ src/main/platform/firebase/typeahead.ts
@@ -26,7 +26,7 @@ import moment from 'moment';
 const entryService = new EntryService(new AcceptTokens(new NounAcceptor(new TemporalAcceptor(new RelativeTemporalAcceptor, new YobiAcceptor(), new NumberUnitAcceptor()), new EnumrateAcceptor(), new NumericNounAcceptor(), new FromToAcceptor(), new AmbiguousDateAcceptor()), new ParticleAcceptor(new AndAcceptor(), new RangeStartAcceptor(), new RangeEndAcceptor())), new KuromojiService());
 const outputService = new OutputService(new OperateDates());
 
-module.exports = functions.https.onRequest((request, response) => {
+module.exports = functions.region('asia-northeast1').https.onRequest((request, response) => {
     const input = request.query['input'];
     const baseDateStr = request.query['baseDate'];
     const baseDate = baseDateStr ? moment(baseDateStr) : moment();
